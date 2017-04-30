4d3b8b604ecc2cb89c17943f588a5b4340ff6599
Taylor A. Liss <tayloraliss@gmail.com>
2017-04-30 11:54:13 -0400

    move broken import

--- Twinty/app/src/main/java/com/example/samort7/androidtwinty/MainActivity.java
+++ Twinty/app/src/main/java/com/example/samort7/androidtwinty/MainActivity.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
